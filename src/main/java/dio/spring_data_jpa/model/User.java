d127c1fafc99ec7b8faea3f14c2fb55b62c6e2c1
tendypora <tendypora@gmail.com>
2025-02-24 22:09:26 -0300

    Att

--- src/main/java/dio/spring_data_jpa/model/User.java
+++ src/main/java/dio/spring_data_jpa/model/User.java
@@ -5,8 +5,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Column;   
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
